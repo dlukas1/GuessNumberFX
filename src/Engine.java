fb072aa8217f08615bce233a16e329b9876be31b
dlukas1 <dlukas@itcollege.ee>
2016-11-15 08:09:39 +0200

    almost done!

--- src/Engine.java
+++ src/Engine.java
@@ -2,17 +2,37 @@
  * Created by Dmitry on 8.11.2016.
  */
 public class Engine {
-    //dat novoe imja !=sisNum
 
-    public int sisNum;
-//zamenit sisNum na "random"
-    public Engine(){
-        this.sisNum = GenerateNumber();
+
+    public int rndNum;
+
+    public Engine() {
+        GenerateNumber();
     }
 
-    public int GenerateNumber() {return (int)(Math.random()*100);}
+    public void GenerateNumber() {
+        this.rndNum = (int) (Math.random() * 100);
+    }
 
-    public String Check (int a) {
-        if(a > this.sisNum) {return "Sinu number on liiga suur";
-    } else if (a == this.sisNum) {return "Võit!";
-} else {return "Sinu number on liiga vaike";}}}
+    public String Check(int a) {
+        if (a > this.rndNum) {
+            return "Sinu number on liiga suur";
+        } else if (a == this.rndNum) {
+            return "Võit!";
+        } else if (a > 100) {
+            return "Number ei saa olla suurem kui 100!";
+        } else if (a <= 0) {
+            return "Number ei saa olla 0 või vähem!";
+        } else {
+            return "Sinu number on liiga vaike";
+        }
+    }
+    public boolean kasNumber (String a) {
+        try {
+            Integer.parseInt(a);
+        } catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+}
--- src/Guess.java
+++ src/Guess.java
@@ -1,8 +1,6 @@
 /**
  * Created by Dmitry on 8.11.2016.
  */
-
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,93 +10,123 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Guess extends Application{
 
+public class Guess extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        //Создаем сцену
         StackPane stack = new StackPane();
         Scene scene = new Scene(stack, 400, 200);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Guess Number"); //Название
+        primaryStage.setResizable(false); //Постоянный размен
 
-        primaryStage.setTitle("Guess Number");
-        primaryStage.setResizable(false);
-
+        //Поле ввода текста
         TextField field1 = new TextField("Sisesta number 1 - 100:");
         stack.getChildren().add(field1);
 
+        //Создаем кнопку
         Button btn = new Button();
-        btn.setText("Start");
-        stack.getChildren().add(btn);
+        btn.setText("Go!");
+        stack.getChildren().add(btn);//добавим на поле
         btn.setTranslateY(50);
 
+        //Кнопка "Играть еще"
         Button btn1 = new Button();
         btn1.setText("Play again");
         btn1.setTranslateY(50);
+        btn1.setTranslateX(100);
 
+        //Инструкция над полем ввода (Удалить после начала игры)
         Label l = new Label("Proovi välja arvata number 1st 100ni! Sul on 5 katset!");
         stack.getChildren().add(l);
         l.setTranslateY(-70);
 
 
-       final Text actiontarget = new Text();
+       //Комментарии внизу - больше, меньше, угадал, проиграл
+        final Text actiontarget = new Text();
         stack.getChildren().add(actiontarget);
         actiontarget.setTranslateY(80);
 
+
+
+        //Кнопка перезапускает игру после проигрыша
         btn1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) {
-               //dodelat'
-            }
+            public void handle(ActionEvent event){}
+            //dodelat'
+
+
         });
 
+        //Начинем игру
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            int i = 0;
+            int i = 0; //0 попыток использовано
             Engine engineObject = new Engine();
 
             @Override
             public void handle(ActionEvent event) {
-                int sisNum;
-                int tryNumber = 5;
 
+                int sisNum;
+                int tryNumber = 4;
+                btn.setText("Go");
+                stack.getChildren().remove(l);//удаляем инструкцию после начала игры
+
+
+
+
+
+                //при проигрыше, если больше 5 попыток:
                 if(i >= tryNumber){
-                    actiontarget.setText("You lost the game! Correct answer was " + engineObject.sisNum);
-                    stack.getChildren().remove(btn);
-                    stack.getChildren().add(btn1);
+                    actiontarget.setFill(Color.RED);
+                    actiontarget.setText("You lost the game! Correct answer was " + engineObject.rndNum);
+                    btn.setText("Start again");
+                    engineObject.GenerateNumber();
+                    i = 0;
+                    //поменять текст на НАЧАТЬ ЗАНОВО и
+                    // stack.getChildren().remove(btn);
+                    //stack.getChildren().add(btn1);
                     //knopka start new game
+
+
 
                 } else {
                     //proverka
-                    i++;
-                    //try catch block
-                    //ESLI VVEL >100 ILI <1
-                    //esli vvel to ze samoe
-                    sisNum = Integer.parseInt(field1.getText());
-                    String answer = engineObject.Check(sisNum);
-                    if(answer == "Võit!"){
-                        actiontarget.setFill(Color.BLUE);
-                        actiontarget.setText("Yeeee, correct");
-                        stack.getChildren().remove(btn);
+                    boolean isNumber= engineObject.kasNumber(field1.getText());
+                    if(isNumber == false){
+                        actiontarget.setText("See ei ole number, sisesta number!");
+                    } else{
+                        sisNum = Integer.parseInt(field1.getText());
 
-                    } else {
-                        actiontarget.setFill(Color.BLUE);
-                        actiontarget.setText(answer + ", left " + (tryNumber - i + 1) + " tries!");
+                        if(sisNum>100) {
+                            actiontarget.setText("Number ei saa olla suurem kui 100!");
+                        } else{
+                            i++;
+                            String answer = engineObject.Check(sisNum);
+
+                            //Если угадал
+                            if(answer == "Võit!"){
+                                actiontarget.setFill(Color.RED);
+                                actiontarget.setText("Yoy won! Congratulations!");
+                                //stack.getChildren().remove(btn);
+
+                                //если не угадал
+                            }
+                            else {
+                                actiontarget.setFill(Color.BLUE);
+                                actiontarget.setText(answer + ", left " + (tryNumber - i+1) + " tries!");//
+                            }
+                        }
 
                     }
                 }
             }
         });
-
-
-
-
-
-
         primaryStage.show();
-
     }
 }
